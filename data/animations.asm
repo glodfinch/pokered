00a881876260039c932504b35cb303cfe52f309b
Nathan Finch <11164339+glodfinch@users.noreply.github.com>
2018-09-09 20:17:43 +0200

    convert subanim headers to binary

--- data/animations.asm
+++ data/animations.asm
@@ -1418,17 +1418,17 @@ SubanimationSketch1:
 	;db $7b,$C6,$00
 
 Subanimation04:
-	db $43
+	db %01000011
 	db $02,$1a,$00
 	db $02,$10,$00
 	db $02,$03,$00
 
 Subanimation05:
-	db $41
+	db %01000001
 	db $02,$10,$00
 
 Subanimation08:
-	db $0b
+	db %00001011
 	db $03,$30,$00
 	db $03,$44,$00
 	db $03,$94,$00
@@ -1442,7 +1442,7 @@ Subanimation08:
 	db $03,$34,$00
 
 Subanimation07:
-	db $0b
+	db %00001011
 	db $03,$30,$00
 	db $03,$a2,$00
 	db $03,$31,$00
@@ -1456,7 +1456,7 @@ Subanimation07:
 	db $03,$34,$00
 
 Subanimation06:
-	db $0b
+	db %00001011
 	db $03,$30,$00
 	db $03,$a2,$00
 	db $03,$93,$00
@@ -1470,14 +1470,14 @@ Subanimation06:
 	db $03,$34,$00
 
 Subanimation09:
-	db $04
+	db %00000100
 	db $03,$21,$04
 	db $04,$21,$04
 	db $03,$21,$04
 	db $05,$21,$04
 
 Subanimation0a:
-	db $46
+	db %01000110
 	db $06,$1b,$00
 	db $07,$1b,$00
 	db $08,$36,$00
@@ -1486,18 +1486,18 @@ Subanimation0a:
 	db $0a,$15,$00
 
 Subanimation0b:
-	db $04
+	db %00000100
 	db $01,$2d,$00
 	db $03,$2f,$00
 	db $03,$35,$00
 	db $03,$4d,$00
 
 Subanimation55:
-	db $41
+	db %01000001
 	db $01,$9d,$00
 
 Subanimation11:
-	db $4c
+	db %01001100
 	db $0b,$26,$00
 	db $0c,$26,$00
 	db $0b,$26,$00
@@ -1512,7 +1512,7 @@ Subanimation11:
 	db $0c,$27,$00
 
 Subanimation2b:
-	db $4b
+	db %01001011
 	db $0d,$03,$03
 	db $0e,$03,$03
 	db $0f,$03,$00
@@ -1526,7 +1526,7 @@ Subanimation2b:
 	db $11,$1b,$00
 
 Subanimation2c:
-	db $4c
+	db %01001100
 	db $12,$01,$00
 	db $12,$0f,$00
 	db $12,$1b,$00
@@ -1541,7 +1541,7 @@ Subanimation2c:
 	db $16,$38,$00
 
 Subanimation12:
-	db $69
+	db %01101001
 	db $17,$30,$00
 	db $17,$39,$00
 	db $17,$3a,$00
@@ -1553,35 +1553,35 @@ Subanimation12:
 	db $17,$1f,$00
 
 Subanimation00:
-	db $41
+	db %01000001
 	db $01,$17,$00
 
 Subanimation01:
-	db $42
+	db %01000010
 	db $01,$0f,$00
 	db $01,$1d,$00
 
 Subanimation02:
-	db $43
+	db %01000011
 	db $01,$12,$00
 	db $01,$15,$00
 	db $01,$1c,$00
 
 Subanimation03:
-	db $44
+	db %01000100
 	db $01,$0b,$00
 	db $01,$11,$00
 	db $01,$18,$00
 	db $01,$1d,$00
 
 Subanimation0c:
-	db $43
+	db %01000011
 	db $0c,$20,$00
 	db $0c,$21,$00
 	db $0c,$23,$00
 
 Subanimation0d:
-	db $46
+	db %01000110
 	db $0c,$20,$02
 	db $0c,$15,$00
 	db $0c,$21,$02
@@ -1590,7 +1590,7 @@ Subanimation0d:
 	db $0c,$19,$00
 
 Subanimation0e:
-	db $49
+	db %01001001
 	db $0c,$20,$02
 	db $0c,$15,$02
 	db $0c,$07,$00
@@ -1602,7 +1602,7 @@ Subanimation0e:
 	db $0c,$0c,$00
 
 Subanimation1f:
-	db $85
+	db %10000101
 	db $0c,$30,$03
 	db $0c,$40,$03
 	db $0c,$41,$03
@@ -1610,7 +1610,7 @@ Subanimation1f:
 	db $0c,$21,$00
 
 Subanimation2e:
-	db $2e
+	db %00101110
 	db $18,$43,$02
 	db $75,$52,$04
 	db $19,$43,$02
@@ -1627,14 +1627,14 @@ Subanimation2e:
 	db $75,$1b,$00
 
 Subanimation2f:
-	db $44
+	db %01000100
 	db $1f,$24,$00
 	db $20,$20,$00
 	db $21,$1a,$00
 	db $22,$15,$00
 
 Subanimation30:
-	db $52
+	db %01010010
 	db $23,$00,$02
 	db $23,$02,$02
 	db $23,$04,$00
@@ -1655,7 +1655,7 @@ Subanimation30:
 	db $24,$28,$00
 
 Subanimation0f:
-	db $4c
+	db %01001100
 	db $26,$0e,$02
 	db $26,$16,$02
 	db $26,$1c,$00
@@ -1670,7 +1670,7 @@ Subanimation0f:
 	db $29,$1c,$00
 
 Subanimation16:
-	db $4c
+	db %01001100
 	db $2a,$05,$00
 	db $2b,$05,$02
 	db $2b,$0c,$02
@@ -1685,7 +1685,7 @@ Subanimation16:
 	db $2c,$00,$00
 
 Subanimation10:
-	db $88
+	db %10001000
 	db $2d,$44,$00
 	db $2e,$45,$00
 	db $2d,$46,$00
@@ -1696,7 +1696,7 @@ Subanimation10:
 	db $2e,$1a,$00
 
 Subanimation31:
-	db $2a
+	db %00101010
 	db $2f,$46,$00
 	db $2f,$4a,$00
 	db $2f,$4b,$00
@@ -1709,7 +1709,7 @@ Subanimation31:
 	db $2f,$51,$00
 
 Subanimation13:
-	db $86
+	db %10000110
 	db $30,$31,$00
 	db $30,$32,$00
 	db $30,$92,$00
@@ -1718,7 +1718,7 @@ Subanimation13:
 	db $30,$10,$00
 
 Subanimation14:
-	db $49
+	db %01001001
 	db $30,$10,$00
 	db $30,$10,$03
 	db $31,$1c,$04
@@ -1730,7 +1730,7 @@ Subanimation14:
 	db $31,$27,$00
 
 Subanimation41:
-	db $85
+	db %10000101
 	db $03,$31,$00
 	db $03,$32,$00
 	db $03,$92,$00
@@ -1738,32 +1738,32 @@ Subanimation41:
 	db $03,$10,$00
 
 Subanimation42:
-	db $43
+	db %01000011
 	db $48,$08,$00
 	db $49,$08,$00
 	db $5a,$08,$00
 
 Subanimation15:
-	db $22
+	db %00100010
 	db $35,$52,$00
 	db $35,$53,$00
 
 Subanimation17:
-	db $44
+	db %01000100
 	db $36,$54,$00
 	db $36,$55,$00
 	db $37,$56,$00
 	db $37,$57,$00
 
 Subanimation18:
-	db $a4
+	db %10100100
 	db $36,$54,$00
 	db $36,$55,$00
 	db $37,$56,$00
 	db $37,$57,$00
 
 Subanimation40:
-	db $46
+	db %01000110
 	db $17,$54,$00
 	db $17,$55,$00
 	db $17,$0e,$00
@@ -1772,7 +1772,7 @@ Subanimation40:
 	db $17,$13,$00
 
 Subanimation19:
-	db $8c
+	db %10001100
 	db $38,$31,$00
 	db $39,$31,$00
 	db $38,$32,$00
@@ -1787,7 +1787,7 @@ Subanimation19:
 	db $39,$10,$00
 
 Subanimation1a:
-	db $50
+	db %01010000
 	db $3a,$08,$00
 	db $3b,$08,$00
 	db $3c,$08,$00
@@ -1806,20 +1806,20 @@ Subanimation1a:
 	db $3f,$0b,$00
 
 Subanimation1b:
-	db $84
+	db %10000100
 	db $40,$31,$00
 	db $40,$32,$00
 	db $40,$92,$00
 	db $40,$15,$00
 
 Subanimation1c:
-	db $43
+	db %01000011
 	db $41,$58,$00
 	db $41,$59,$00
 	db $41,$21,$00
 
 Subanimation1d:
-	db $af
+	db %10101111
 	db $24,$9a,$00
 	db $23,$1b,$02
 	db $24,$22,$00
@@ -1837,22 +1837,22 @@ Subanimation1d:
 	db $25,$99,$03
 
 Subanimation1e:
-	db $01
+	db %00000001
 	db $25,$75,$00
 
 Subanimation20:
-	db $42
+	db %01000010
 	db $42,$07,$00
 	db $43,$07,$00
 
 Subanimation21:
-	db $43
+	db %01000011
 	db $44,$00,$00
 	db $45,$08,$00
 	db $46,$10,$02
 
 Subanimation22:
-	db $8b
+	db %10001011
 	db $47,$10,$00
 	db $47,$56,$00
 	db $47,$07,$00
@@ -1866,7 +1866,7 @@ Subanimation22:
 	db $47,$b0,$00
 
 Subanimation2d:
-	db $66
+	db %01100110
 	db $44,$64,$00
 	db $45,$65,$00
 	db $46,$66,$00
@@ -1875,15 +1875,15 @@ Subanimation2d:
 	db $47,$66,$00
 
 Subanimation39:
-	db $61
+	db %01100001
 	db $47,$67,$00
 
 Subanimation4e:
-	db $41
+	db %01000001
 	db $71,$0f,$03
 
 Subanimation4f:
-	db $47
+	db %01000111
 	db $71,$0f,$00
 	db $71,$08,$00
 	db $71,$01,$00
@@ -1893,7 +1893,7 @@ Subanimation4f:
 	db $74,$95,$00
 
 Subanimation50:
-	db $48
+	db %01001000
 	db $74,$95,$00
 	db $73,$95,$00
 	db $72,$95,$00
@@ -1904,7 +1904,7 @@ Subanimation50:
 	db $71,$16,$00
 
 Subanimation29:
-	db $5d
+	db %01011101
 	db $48,$0f,$00
 	db $4a,$68,$03
 	db $4b,$2a,$03
@@ -1936,63 +1936,63 @@ Subanimation29:
 	db $49,$6b,$00
 
 Subanimation2a:
-	db $44
+	db %01000100
 	db $4e,$2b,$00
 	db $4f,$2b,$00
 	db $50,$2b,$00
 	db $50,$2b,$00
 
 Subanimation23:
-	db $42
+	db %01000010
 	db $51,$2d,$00
 	db $51,$6e,$00
 
 Subanimation24:
-	db $a2
+	db %10100010
 	db $51,$2d,$00
 	db $51,$6e,$00
 
 Subanimation25:
-	db $62
+	db %01100010
 	db $52,$71,$00
 	db $52,$72,$00
 
 Subanimation26:
-	db $02
+	db %00000010
 	db $52,$01,$00
 	db $52,$2c,$00
 
 Subanimation3a:
-	db $63
+	db %01100011
 	db $53,$71,$00
 	db $53,$7f,$00
 	db $53,$81,$00
 
 Subanimation3b:
-	db $03
+	db %00000011
 	db $53,$01,$00
 	db $53,$15,$00
 	db $53,$2c,$00
 
 Subanimation27:
-	db $a2
+	db %10100010
 	db $54,$01,$00
 	db $54,$2c,$00
 
 Subanimation28:
-	db $23
+	db %00100011
 	db $55,$73,$03
 	db $56,$73,$03
 	db $57,$73,$00
 
 Subanimation32:
-	db $63
+	db %01100011
 	db $47,$74,$00
 	db $47,$43,$00
 	db $47,$75,$00
 
 Subanimation33:
-	db $26
+	db %00100110
 	db $58,$76,$00
 	db $34,$76,$00
 	db $58,$76,$00
@@ -2001,7 +2001,7 @@ Subanimation33:
 	db $34,$76,$00
 
 Subanimation3c:
-	db $67
+	db %01100111
 	db $59,$79,$03
 	db $59,$7b,$03
 	db $59,$77,$03
@@ -2011,7 +2011,7 @@ Subanimation3c:
 	db $59,$76,$00
 
 Subanimation3d:
-	db $08
+	db %00001000
 	db $3a,$4d,$00
 	db $3b,$4d,$00
 	db $3c,$4d,$00
@@ -2022,7 +2022,7 @@ Subanimation3d:
 	db $3f,$4d,$00
 
 Subanimation34:
-	db $35
+	db %00110101
 	db $48,$7d,$00
 	db $49,$7d,$00
 	db $5a,$7d,$00
@@ -2046,14 +2046,14 @@ Subanimation34:
 	db $5a,$82,$00
 
 Subanimation35:
-	db $24
+	db %00100100
 	db $5b,$83,$03
 	db $5c,$84,$03
 	db $5d,$85,$03
 	db $5e,$09,$00
 
 Subanimation36:
-	db $48
+	db %01001000
 	db $5f,$2a,$00
 	db $5f,$00,$00
 	db $60,$2a,$00
@@ -2064,7 +2064,7 @@ Subanimation36:
 	db $62,$00,$00
 
 Subanimation37:
-	db $2a
+	db %00101010
 	db $63,$89,$00
 	db $64,$75,$00
 	db $63,$76,$00
@@ -2077,7 +2077,7 @@ Subanimation37:
 	db $65,$1a,$00
 
 Subanimation38:
-	db $50
+	db %01010000
 	db $66,$8a,$00
 	db $66,$33,$00
 	db $66,$2e,$00
@@ -2096,7 +2096,7 @@ Subanimation38:
 	db $67,$29,$00
 
 Subanimation3e:
-	db $92
+	db %10010010
 	db $02,$31,$00
 	db $34,$31,$00
 	db $02,$31,$00
@@ -2117,7 +2117,7 @@ Subanimation3e:
 	db $02,$10,$00
 
 Subanimation3f:
-	db $72
+	db %01110010
 	db $68,$4b,$00
 	db $68,$8c,$00
 	db $68,$20,$00
@@ -2138,7 +2138,7 @@ Subanimation3f:
 	db $68,$1f,$00
 
 Subanimation44:
-	db $2c
+	db %00101100
 	db $69,$4b,$00
 	db $69,$8c,$00
 	db $69,$20,$00
@@ -2153,20 +2153,20 @@ Subanimation44:
 	db $69,$06,$00
 
 Subanimation43:
-	db $a3
+	db %10100011
 	db $6a,$07,$00
 	db $6b,$0f,$00
 	db $6c,$17,$00
 
 Subanimation45:
-	db $24
+	db %00100100
 	db $6d,$8b,$00
 	db $6d,$84,$00
 	db $6d,$63,$00
 	db $6d,$8c,$00
 
 Subanimation46:
-	db $26
+	db %00100110
 	db $6d,$8b,$00
 	db $6d,$84,$00
 	db $6d,$63,$00
@@ -2175,13 +2175,13 @@ Subanimation46:
 	db $6d,$89,$00
 
 Subanimation47:
-	db $23
+	db %00100011
 	db $06,$82,$00
 	db $07,$82,$00
 	db $08,$96,$00
 
 Subanimation48:
-	db $06
+	db %00000110
 	db $03,$41,$04
 	db $03,$48,$04
 	db $04,$48,$04
@@ -2190,24 +2190,24 @@ Subanimation48:
 	db $03,$48,$03
 
 Subanimation49:
-	db $04
+	db %00000100
 	db $04,$48,$04
 	db $03,$48,$04
 	db $05,$48,$04
 	db $03,$48,$03
 
 Subanimation4a:
-	db $01
+	db %00000001
 	db $04,$84,$03
 
 Subanimation4b:
-	db $03
+	db %00000011
 	db $06,$72,$00
 	db $07,$72,$00
 	db $08,$72,$00
 
 Subanimation4c:
-	db $68
+	db %01101000
 	db $6f,$30,$00
 	db $6e,$30,$00
 	db $70,$30,$00
@@ -2218,7 +2218,7 @@ Subanimation4c:
 	db $6e,$30,$00
 
 Subanimation4d:
-	db $26
+	db %00100110
 	db $32,$4b,$00
 	db $33,$4f,$00
 	db $32,$20,$00
@@ -2227,7 +2227,7 @@ Subanimation4d:
 	db $33,$0d,$00
 
 Subanimation51:
-	db $a6
+	db %10100110
 	db $76,$1b,$00
 	db $34,$1b,$00
 	db $76,$1b,$00
@@ -2236,7 +2236,7 @@ Subanimation51:
 	db $34,$1b,$00
 
 Subanimation52:
-	db $47
+	db %01000111
 	db $77,$25,$00
 	db $77,$9b,$00
 	db $77,$1a,$00
@@ -2246,7 +2246,7 @@ Subanimation52:
 	db $77,$8c,$00
 
 Subanimation53:
-	db $0c
+	db %00001100
 	db $78,$30,$00
 	db $78,$a2,$00
 	db $78,$93,$00
@@ -2261,7 +2261,7 @@ Subanimation53:
 	db $01,$9e,$00
 
 Subanimation54:
-	db $0b
+	db %00001011
 	db $79,$30,$00
 	db $79,$a2,$00
 	db $79,$93,$00
